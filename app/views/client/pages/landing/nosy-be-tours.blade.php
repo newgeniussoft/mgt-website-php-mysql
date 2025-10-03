d3d4e4670533c00ffdfb1c9ee5fa69ae219f7dcf
Gino <georgino197@gmail.com>
2025-10-03 16:25:47 +0300

    intialize new landing page

---
+++ app/views/client/pages/landing/nosy-be-tours.blade.php
@@ -0,0 +1,6 @@
+@import('app/utils/helpers/helper.php')
+@include(client.partials.head)
+<link rel="stylesheet" href="{{ assets('css/landing.css') }}">
+@include(client.partials.header)
+
+@include(client.partials.footer)
