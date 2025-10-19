4f42321292cf04252b013045279c8b5e559dffbe
Gino <georgino197@gmail.com>
2025-10-19 15:36:36 +0300

    Create blade engine

---
+++ app/core/BladeEngine.php
@@ -0,0 +1,366 @@
+<?php
+
+class BladeEngine
+{
+    protected $viewPath;
+    protected $cachePath;
+    protected $extensions = [];
+    
+    public function __construct($viewPath, $cachePath)
+    {
+        $this->viewPath = rtrim($viewPath, '/');
+        $this->cachePath = rtrim($cachePath, '/');
+        
+        if (!is_dir($this->cachePath)) {
+            mkdir($this->cachePath, 0755, true);
+        }
+    }
+    
+    /**
+     * Render a blade template
+     */
+    public function render($view, $data = [])
+    {
+        $viewFile = $this->viewPath . '/' . str_replace('.', '/', $view) . '.blade.php';
+        
+        if (!file_exists($viewFile)) {
+            throw new Exception("View [$view] not found at: $viewFile");
+        }
+        
+        $cacheFile = $this->getCacheFile($viewFile);
+        
+        if (!file_exists($cacheFile) || filemtime($viewFile) > filemtime($cacheFile)) {
+            $compiled = $this->compile(file_get_contents($viewFile));
+            file_put_contents($cacheFile, $compiled);
+        }
+        
+        return $this->evaluate($cacheFile, $data);
+    }
+    
+    /**
+     * Compile blade syntax to PHP
+     */
+    protected function compile($content)
+    {
+        // Apply all compilation methods in order
+        $content = $this->compileExtends($content);
+        $content = $this->compileSections($content);
+        $content = $this->compileYields($content);
+        $content = $this->compileIncludes($content);
+        $content = $this->compileComponents($content);
+        $content = $this->compileStacks($content);
+        $content = $this->compileComments($content);
+        $content = $this->compilePhp($content);
+        $content = $this->compileAuth($content);
+        $content = $this->compileGuest($content);
+        $content = $this->compileProduction($content);
+        $content = $this->compileEnv($content);
+        $content = $this->compileConditionals($content);
+        $content = $this->compileLoops($content);
+        $content = $this->compileSwitchStatements($content);
+        $content = $this->compileJson($content);
+        $content = $this->compileCsrf($content);
+        $content = $this->compileMethod($content);
+        $content = $this->compileRawEchos($content);
+        $content = $this->compileEchos($content);
+        
+        return $content;
+    }
+    
+    /**
+     * Compile {{ $var }} to escaped echo
+     */
+    protected function compileEchos($content)
+    {
+        return preg_replace('/\{\{\s*(.+?)\s*\}\}/s', '<?php echo htmlspecialchars($1 ?? \'\', ENT_QUOTES, \'UTF-8\'); ?>', $content);
+    }
+    
+    /**
+     * Compile {!! $var !!} to raw echo
+     */
+    protected function compileRawEchos($content)
+    {
+        return preg_replace('/\{!!\s*(.+?)\s*!!\}/s', '<?php echo $1; ?>', $content);
+    }
+    
+    /**
+     * Compile {{-- comments --}}
+     */
+    protected function compileComments($content)
+    {
+        return preg_replace('/\{\{--.+?--\}\}/s', '', $content);
+    }
+    
+    /**
+     * Compile @php and @endphp
+     */
+    protected function compilePhp($content)
+    {
+        $content = preg_replace('/\@php\s*\((.+?)\)/s', '<?php $1; ?>', $content);
+        $content = preg_replace('/\@php/', '<?php', $content);
+        $content = preg_replace('/\@endphp/', '?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @if, @elseif, @else, @endif
+     */
+    protected function compileConditionals($content)
+    {
+        $content = preg_replace('/\@if\s*\((.+?)\)/', '<?php if($1): ?>', $content);
+        $content = preg_replace('/\@elseif\s*\((.+?)\)/', '<?php elseif($1): ?>', $content);
+        $content = preg_replace('/\@else/', '<?php else: ?>', $content);
+        $content = preg_replace('/\@endif/', '<?php endif; ?>', $content);
+        
+        // @unless, @endunless
+        $content = preg_replace('/\@unless\s*\((.+?)\)/', '<?php if(!($1)): ?>', $content);
+        $content = preg_replace('/\@endunless/', '<?php endif; ?>', $content);
+        
+        // @isset, @endisset
+        $content = preg_replace('/\@isset\s*\((.+?)\)/', '<?php if(isset($1)): ?>', $content);
+        $content = preg_replace('/\@endisset/', '<?php endif; ?>', $content);
+        
+        // @empty, @endempty
+        $content = preg_replace('/\@empty\s*\((.+?)\)/', '<?php if(empty($1)): ?>', $content);
+        $content = preg_replace('/\@endempty/', '<?php endif; ?>', $content);
+        
+        return $content;
+    }
+    
+    /**
+     * Compile @auth, @guest
+     */
+    protected function compileAuth($content)
+    {
+        $content = preg_replace('/\@auth(\s*\((.+?)\))?/', '<?php if(isset($__auth) && $__auth): ?>', $content);
+        $content = preg_replace('/\@endauth/', '<?php endif; ?>', $content);
+        return $content;
+    }
+    
+    protected function compileGuest($content)
+    {
+        $content = preg_replace('/\@guest/', '<?php if(!isset($__auth) || !$__auth): ?>', $content);
+        $content = preg_replace('/\@endguest/', '<?php endif; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @production, @env
+     */
+    protected function compileProduction($content)
+    {
+        $content = preg_replace('/\@production/', '<?php if(getenv(\'APP_ENV\') === \'production\'): ?>', $content);
+        $content = preg_replace('/\@endproduction/', '<?php endif; ?>', $content);
+        return $content;
+    }
+    
+    protected function compileEnv($content)
+    {
+        $content = preg_replace('/\@env\s*\((.+?)\)/', '<?php if(getenv(\'APP_ENV\') === $1): ?>', $content);
+        $content = preg_replace('/\@endenv/', '<?php endif; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile loops: @foreach, @for, @while, @forelse
+     */
+    protected function compileLoops($content)
+    {
+        // @foreach
+        $content = preg_replace('/\@foreach\s*\((.+?)\)/', '<?php foreach($1): ?>', $content);
+        $content = preg_replace('/\@endforeach/', '<?php endforeach; ?>', $content);
+        
+        // @forelse
+        $content = preg_replace('/\@forelse\s*\((.+?)\s+as\s+(.+?)\)/', '<?php if(!empty($1)): foreach($1 as $2): ?>', $content);
+        $content = preg_replace('/\@empty/', '<?php endforeach; else: ?>', $content);
+        $content = preg_replace('/\@endforelse/', '<?php endif; ?>', $content);
+        
+        // @for
+        $content = preg_replace('/\@for\s*\((.+?)\)/', '<?php for($1): ?>', $content);
+        $content = preg_replace('/\@endfor/', '<?php endfor; ?>', $content);
+        
+        // @while
+        $content = preg_replace('/\@while\s*\((.+?)\)/', '<?php while($1): ?>', $content);
+        $content = preg_replace('/\@endwhile/', '<?php endwhile; ?>', $content);
+        
+        // @continue, @break
+        $content = preg_replace('/\@continue(\s*\((.+?)\))?/', '<?php if(empty($2) || $2) continue; ?>', $content);
+        $content = preg_replace('/\@break(\s*\((.+?)\))?/', '<?php if(empty($2) || $2) break; ?>', $content);
+        
+        return $content;
+    }
+    
+    /**
+     * Compile @switch, @case, @default
+     */
+    protected function compileSwitchStatements($content)
+    {
+        $content = preg_replace('/\@switch\s*\((.+?)\)/', '<?php switch($1): ?>', $content);
+        $content = preg_replace('/\@case\s*\((.+?)\)/', '<?php case $1: ?>', $content);
+        $content = preg_replace('/\@default/', '<?php default: ?>', $content);
+        $content = preg_replace('/\@endswitch/', '<?php endswitch; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @extends
+     */
+    protected function compileExtends($content)
+    {
+        preg_match('/\@extends\s*\([\'"](.+?)[\'"]\)/', $content, $matches);
+        
+        if (!empty($matches)) {
+            $layout = $matches[1];
+            $content = preg_replace('/\@extends\s*\([\'"](.+?)[\'"]\)/', '<?php $__layout = "' . $layout . '"; ?>', $content);
+        }
+        
+        return $content;
+    }
+    
+    /**
+     * Compile @section and @endsection
+     */
+    protected function compileSections($content)
+    {
+        // @section inline (process first)
+        $content = preg_replace('/\@section\s*\([\'"](.+?)[\'"]\s*,\s*[\'"](.+?)[\'"]\)/', '<?php $__sections["$1"] = "$2"; ?>', $content);
+        
+        // @section with content
+        $content = preg_replace('/\@section\s*\([\'"](.+?)[\'"]\)/', '<?php $__currentSection = "$1"; ob_start(); ?>', $content);
+        $content = preg_replace('/\@endsection/', '<?php $__sections[$__currentSection] = ob_get_clean(); ?>', $content);
+        
+        return $content;
+    }
+    
+    /**
+     * Compile @yield
+     */
+    protected function compileYields($content)
+    {
+        $content = preg_replace('/\@yield\s*\([\'"](.+?)[\'"](?:\s*,\s*[\'"](.+?)[\'"]\s*)?\)/', '<?php echo $__sections["$1"] ?? "$2"; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @include
+     */
+    protected function compileIncludes($content)
+    {
+        $content = preg_replace_callback('/\@include\s*\([\'"](.+?)[\'"](?:\s*,\s*(\[.+?\]))?\)/', function ($matches) {
+            $view = $matches[1];
+            $data = $matches[2] ?? '[]';
+            return '<?php echo $this->render("' . $view . '", array_merge(get_defined_vars(), ' . $data . ')); ?>';
+        }, $content);
+        
+        return $content;
+    }
+    
+    /**
+     * Compile @component
+     */
+    protected function compileComponents($content)
+    {
+        $content = preg_replace('/\@component\s*\([\'"](.+?)[\'"]\)/', '<?php $this->startComponent("$1"); ?>', $content);
+        $content = preg_replace('/\@endcomponent/', '<?php echo $this->renderComponent(); ?>', $content);
+        $content = preg_replace('/\@slot\s*\([\'"](.+?)[\'"]\)/', '<?php $this->slot("$1"); ?>', $content);
+        $content = preg_replace('/\@endslot/', '<?php $this->endSlot(); ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @push, @stack
+     */
+    protected function compileStacks($content)
+    {
+        $content = preg_replace('/\@push\s*\([\'"](.+?)[\'"]\)/', '<?php ob_start(); ?>', $content);
+        $content = preg_replace('/\@endpush/', '<?php $__stacks["$1"][] = ob_get_clean(); ?>', $content);
+        $content = preg_replace('/\@stack\s*\([\'"](.+?)[\'"]\)/', '<?php echo implode("", $__stacks["$1"] ?? []); ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @json
+     */
+    protected function compileJson($content)
+    {
+        $content = preg_replace('/\@json\s*\((.+?)\)/', '<?php echo json_encode($1); ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @csrf
+     */
+    protected function compileCsrf($content)
+    {
+        $content = preg_replace('/\@csrf/', '<?php echo \'<input type="hidden" name="_token" value="\' . ($__csrf ?? \'\') . \'">\'; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Compile @method
+     */
+    protected function compileMethod($content)
+    {
+        $content = preg_replace('/\@method\s*\([\'"](.+?)[\'"]\)/', '<?php echo \'<input type="hidden" name="_method" value="$1">\'; ?>', $content);
+        return $content;
+    }
+    
+    /**
+     * Get cache file path
+     */
+    protected function getCacheFile($viewFile)
+    {
+        return $this->cachePath . '/' . md5($viewFile) . '.php';
+    }
+    
+    /**
+     * Evaluate compiled template
+     */
+    protected function evaluate($path, $data)
+    {
+        $obLevel = ob_get_level();
+        ob_start();
+        
+        extract($data, EXTR_SKIP);
+        
+        // Initialize blade variables
+        if (!isset($__sections)) $__sections = [];
+        if (!isset($__stacks)) $__stacks = [];
+        if (!isset($__layout)) $__layout = null;
+        if (!isset($__currentSection)) $__currentSection = null;
+        
+        try {
+            include $path;
+            
+            // If layout exists, render it
+            if (isset($__layout) && $__layout) {
+                ob_end_clean(); // Clear current buffer
+                return $this->render($__layout, array_merge($data, [
+                    '__sections' => $__sections,
+                    '__stacks' => $__stacks
+                ]));
+            }
+            
+        } catch (Exception $e) {
+            while (ob_get_level() > $obLevel) {
+                ob_end_clean();
+            }
+            throw $e;
+        }
+        
+        return ob_get_clean();
+    }
+    
+    /**
+     * Clear all cached views
+     */
+    public function clearCache()
+    {
+        $files = glob($this->cachePath . '/*.php');
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+    }
+}
