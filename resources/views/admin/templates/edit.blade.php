bc4edd12511877a5657dec2582b4fbe0fca29570
Gino <georgino197@gmail.com>
2025-11-11 11:51:14 +0200

    Refacto 4/4

--- resources/views/admin/templates/edit.blade.php
+++ resources/views/admin/templates/edit.blade.php
@@ -21,34 +21,30 @@
                 <div class="sidebar">
                     <div class="sidebar-icon active" onclick="togglePanel('explorer')" title="Explorer (Ctrl+Shift+E)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-  class="lucide lucide-folder">
-  <path d="M4 4h5l2 3h9a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"/>
-</svg>
-
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-folder">
+                            <path d="M4 4h5l2 3h9a2 2 0 0 1 2 2v9a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2z"/>
+                        </svg>
                     </div>
                     <div class="sidebar-icon" onclick="togglePanel('infos')" title="Information">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-  class="lucide lucide-file">
-  <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
-  <polyline points="14 2 14 8 20 8"/>
-</svg>
-
-                        
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-file">
+                            <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
+                            <polyline points="14 2 14 8 20 8"/>
+                        </svg>
                     </div>
                     <div class="sidebar-icon" onclick="togglePanel('extensions')" title="Extensions (Ctrl+Shift+X)">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"
-  stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-  class="lucide lucide-list">
-  <line x1="8" y1="6" x2="21" y2="6"/>
-  <line x1="8" y1="12" x2="21" y2="12"/>
-  <line x1="8" y1="18" x2="21" y2="18"/>
-  <line x1="3" y1="6" x2="3" y2="6"/>
-  <line x1="3" y1="12" x2="3" y2="12"/>
-  <line x1="3" y1="18" x2="3" y2="18"/>
-</svg>
-
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                            class="lucide lucide-list">
+                            <line x1="8" y1="6" x2="21" y2="6"/>
+                            <line x1="8" y1="12" x2="21" y2="12"/>
+                            <line x1="8" y1="18" x2="21" y2="18"/>
+                            <line x1="3" y1="6" x2="3" y2="6"/>
+                            <line x1="3" y1="12" x2="3" y2="12"/>
+                            <line x1="3" y1="18" x2="3" y2="18"/>
+                        </svg>
                     </div>
                 </div>
                 <!-- Side Panel -->
